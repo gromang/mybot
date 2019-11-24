bec9711669e538b72f33b86403376f5ce0fa2efb
Moiseev Roman <lp15.mr@yandex.ru>
2019-11-24 09:07:07 +0300

    Still not working

---
+++ bot.py
@@ -0,0 +1,31 @@
+import logging
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+PROXY = {'proxy_url': 'socks5://t1.learn.python.ru:1080',
+         'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
+
+logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO, filename='bot.log')
+
+
+def main():
+    mybot = Updater(
+        "598372667:AAG63-I4p59gi2Jp8IWBGK-KLN1wcyx_1nA", request_kwargs=PROXY)
+    dp = mybot.dispatcher
+    dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+    mybot.start_polling()
+    mybot.idle()
+
+
+def greet_user(bot, update):
+    print(update)
+    text = 'Вызван /start'
+    print(text)
+    update.message.reply_text(text)
+
+
+def talk_to_me(bot, update):
+    user_text = update.message.text
+    print(user_text)
+    update.message.reply_text(user_text)
