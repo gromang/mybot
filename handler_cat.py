173633ab65ab9931f4e1818dabdca16632d278ce
Roman Moiseev <lp15.mr@yandex.ru>
2019-12-06 16:03:47 +0300

    chat_id > chat.id

--- handler_cat.py
+++ handler_cat.py
@@ -4,4 +4,4 @@ from random import choice
 def send_cat_picture(update, context):
     cat_list = glob('cats/*cat*.pn*')
     cat_pic = choice(cat_list)
-    context.bot.send_photo(chat_id=update.message.chat_id, photo=open(cat_pic, 'rb'))
+    context.bot.send_photo(chat_id=update.message.chat.id, photo=open(cat_pic, 'rb'))
